c383da9cf6d3f5a019081536dfb94d2d7c81673f
KarimKuropka <152939809+KarimKuropka@users.noreply.github.com>
2025-12-02 18:14:53 +0100

    Advent of Code Day 2

--- src/test/kotlin/AdventOfCode2025.kt
+++ src/test/kotlin/AdventOfCode2025.kt
@@ -5,7 +5,7 @@ import java.io.File
 
 class AdventOfCode2025 {
 
-// ---------- Day 1 ---------- //
+    // ---------- Day 1 ---------- //
     @Test
     fun findEntranceCodePart1() {
         val input = File("src/test/resources/2025advent1.txt").readLines()
@@ -38,20 +38,74 @@ class AdventOfCode2025 {
         for (line in input) {
             val direction = line[0]
             val amount = line.substring(1).toInt()
+            counter += amount / 100
 
             recentState = when (direction) {
                 'R' -> {
-                    counter += amount / 100
                     if (recentState + (amount % 100) >= 100) counter++
                     (recentState + amount) % 100
                 }
                 else -> {
-                    counter += amount / 100
                     if (recentState != 0 && (recentState - (amount % 100) <= 0)) counter++
                     (recentState - amount).mod(100)
                 }
             }
         }
         println(counter)
+    }
+
+    // ---------- Day 2 ---------- //
+    @Test
+    fun sortOutIDsPart1() {
+        val input = File("src/test/resources/2025advent2.txt").readLines()[0].split(",")
+            .map { it.split("-") }
+        println(input.joinToString())
+
+        var addedWrongIDs = 0L
+        Loop@ for (pair in input) {
+            val start = pair[0]
+            val end = pair[1]
+
+            // numbers of uneven length per definition can't be wrong, so no need to check the range
+            if (start.length % 2 != 0 && end.length == start.length) continue@Loop
+
+            for (num in start.toLong()..end.toLong()) {
+                // same as above, just skip any uneven-length numbers
+                if ( num.toString().length % 2 != 0) continue
+
+                val numString = num.toString()
+                val firstHalf = numString.substring(0, numString.length / 2)
+                val secondHalf = numString.substring(numString.length / 2)
+
+                if (firstHalf == secondHalf) {
+                    addedWrongIDs += num
+                    println(num)
+                }
+            }
+        }
+        println(addedWrongIDs)
+    }
+
+    @Test
+    fun sortOutIDsPart2() {
+        val input = File("src/test/resources/2025advent2.txt").readLines()[0].split(",")
+            .map { it.split("-") }
+        println(input.joinToString())
+
+        var addedWrongIDs = 0L
+        Loop@ for (pair in input) {
+            val start = pair[0]
+            val end = pair[1]
+
+            for (num in start.toLong()..end.toLong()) {
+                val numString = num.toString()
+                val regex = """([0-9]{1,6})\1+""".toRegex()
+                if (regex.matches(num.toString())) {
+                    addedWrongIDs += num
+                    println(num)
+                }
+            }
+        }
+        println(addedWrongIDs)
     }
 }
